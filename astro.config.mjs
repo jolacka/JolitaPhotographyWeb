03b4d7422750e2cda1a18fa7b3e828fb0dc33bb1
paske <paskeviciute@gmail.com>
2024-09-27 13:17:41 +0100

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://jolacka.github.io/JolitaPhotographyWeb/",
+  //site: "https://jolacka.github.io/JolitaPhotographyWeb/", - 1first attempt
+  site: "https://jolacka.github.io/Jolita-s-PHOTOGRAPHY/",
   base: "/JolitaPhotographyWeb/", // GitHub Pages repository name
 });
